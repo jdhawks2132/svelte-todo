21cf01d37df22c76758202a4708e3d94e0c6028c
snl95 <snl95@txstate.edu>
2022-05-05 11:04:24 -0500

    added basic styling and todoform comp

---
+++ src/lib/TodoForm.svelte
@@ -0,0 +1,22 @@
+<script>
+	let todo = '';
+	const handleSubmit = () => {
+		console.log('submitting');
+	};
+</script>
+
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
+	<div class="flex flex-col text-sm mb-2">
+		<label class="font-bold mb-2 text-gray-800" for="todo">Todo</label>
+		<input
+			class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
+			type="text"
+			name="todo"
+			placeholder="Add Todo..."
+			bind:value={todo}
+		/>
+	</div>
+	<button type="submit" class="w-full shadow-sm rounded bg-blue-500 text-white py-2 px-4"
+		>Submit</button
+	>
+</form>
